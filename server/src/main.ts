844bec68bc1229e1eca0cd870f1cb8224473a9ec
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2025-12-28 17:50:15 +0500

    Added hardcode frontend url

--- server/src/main.ts
+++ server/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
 
   // CORS configuration
   app.enableCors({
-    origin: [frontendUrl, 'http://localhost:3000', 'http://localhost:5173'],
+    origin: [frontendUrl, 'http://localhost:5173', 'https://edikit.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: [
